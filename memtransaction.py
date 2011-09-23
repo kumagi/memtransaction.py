beddfc7dc70fa18aa19874729ccfd37fc7357312
Hiroki Kumzaki <hiroki.kumazaki@gmail.com>
2011-09-24 02:09:19 +0900

    decorator disabled

--- memtransaction.py
+++ memtransaction.py
@@ -1,32 +1,32 @@
 # -*- coding: utf-8 -*-
-
 import time
 import memcache
+import traceback,sys
 
 from time import sleep
 from random import randint
-
-
 
 class AbortException(Exception):
     pass
 
 def read_commited(old, new, status):
     if status == 'commited':
-        return new
+        return new, old
     elif status == 'abort' or status == 'active':
-        return old
+        return old, new
     else:
         raise Exception('invalid status' + status)
 def read_repeatable(old, new, status):
     if status == 'commited':
-        return new
+        return new,old
     elif status == 'abort':
-        return old
+        return old,new
     elif status == 'active':
         return None
     else:
         raise Exception('invalid status' + status)
+def async_delete(client, target):
+    client.delete(target)
 
 class MemTr(object):
     """ transaction on memcached """
@@ -55,7 +55,7 @@ class MemTr(object):
         status = self.mc.gets(self.transaction_status)
         if status != 'active': raise AbortException
         return self.mc.cas(self.transaction_status, 'commited')
-    class resolver():
+    class resolver(object):
         def __init__(self, mc):
             self.count = 10
             self.mc = mc
@@ -69,7 +69,6 @@ class MemTr(object):
                 self.mc.cas(other_status, 'abort')
 
     def set(self, key, value):
-        new_key = self.add_random(value)
         resolver = self.resolver(self.mc)
         while 1:
             old = new = status_name = None
@@ -77,6 +76,7 @@ class MemTr(object):
                 #print "set:",self.mc.gets(key)
                 old, new, status_name = self.mc.gets(key)
             except TypeError:
+                new_key = self.add_random(value)
                 if self.mc.add(key,[None, new_key, self.transaction_status]):
                     break
                 continue
@@ -84,29 +84,30 @@ class MemTr(object):
             if status_name == self.transaction_status:
                 self.cache[key] = value
                 if old == new:
+                    new_key = self.add_random(value)
                     if self.mc.cas(key, [old, new_key, self.transaction_status]):
                         break
                 else:
                     if not self.mc.replace(new, value):
-                        #print 'abort!!'
                         raise AbortException
-                    else:
-                        self.mc.delete(new_key)
                     break
             else:
                 state = self.mc.get(status_name)
-                next_old = read_repeatable(old,new,state)
-                if next_old == None:
-                    #print 'set: contention'
+                try:
+                    next_old, to_delete = read_repeatable(old,new,state)
+                except TypeError:
                     resolver(status_name)
                     continue
-                #print "set: cas for ", [next_old, new_key, self.transaction_status]
+                # print "set: cas for ", [next_old, new_key, self.transaction_status]
+                new_key = self.add_random(value)
                 result = self.mc.cas(key,[next_old, new_key, self.transaction_status])
                 if result:
                     self.cache[key] = value
+                    if to_delete != None and to_delete != next_old:
+                        self.mc.delete(to_delete)
                     #print "setting cache:",self.cache
                     break
-    def get(self, key):
+    def get_commited(self, key):
         while 1:
             old = new = status_name = None
             try:
@@ -115,23 +116,19 @@ class MemTr(object):
             except TypeError:
                 #print "typeerror"
                 return None  # read commited!!
+
             if status_name == self.transaction_status:
-                #print 'cache hit'
                 return self.cache[key]
                 #return self.mc.get(new)
             else:
                 state = self.mc.get(status_name)
-                if state == 'commited':
-                    commited_value, to_delete = new, old
-                elif state == 'abort':
-                    commited_value, to_delete = old, new
-                elif state == 'active':
-                    commited_value, to_delete = old, None
-                else:
-                    raise Exception
+                commited_value, to_delete = read_commited(old,new,state)
+                if state != 'active':
+                    self.mc.delete(to_delete)
                 result = self.mc.get(commited_value)
                 self.cache[key] = result
                 return result
+
     def get_repeatable(self, key):
         resolver = self.resolver(self.mc)
         while 1:
@@ -148,53 +145,56 @@ class MemTr(object):
                 #return self.mc.get(new)
             else:
                 state = self.mc.get(status_name)
-                if state == 'commited':
-                    commited_value, to_delete = new, old
-                elif state == 'abort':
-                    commited_value, to_delete = old, new
-                elif state == 'active':
-                    resolver(status_name)
-                    continue
-                else:
-                    raise Exception
+                commited_value, to_delete = read_repeatable(old,new,state)
                 if self.mc.cas(key, [commited_value,
                                      commited_value,
                                      self.transaction_status]):
-                    if to_delete != None and new != old:
+                    if state != 'active' and new != old:
                         self.mc.delete(to_delete)
                 else:
                     continue
                 result = self.mc.get(commited_value)
                 self.cache[key] = result
                 return result
-def transaction(kvs):
-    def inner_transaction(tr):
-        def impl():
-            transaction = MemTr(kvs)
-            setter = lambda k,v : transaction.set(k,v)
-            getter = lambda k :   transaction.get_repeatable(k)
-            while(1):
-                transaction.begin()
-                try:
-                    tr(setter, getter)
-                    if transaction.commit() == True:
-                        return transaction.cache
-                except AbortException:
-                    continue
-        return impl()
-    return inner_transaction
+
+def rr_transaction(kvs, target_transaction):
+    transaction = MemTr(kvs)
+    setter = lambda k,v : transaction.set(k,v)
+    getter = lambda k :   transaction.get_repeatable(k)
+    while(1):
+        transaction.begin()
+        try:
+            target_transaction(setter, getter)
+            if transaction.commit() == True:
+                return transaction.cache
+        except AbortException:
+            continue
+def rc_transaction(kvs, target_transaction):
+    transaction = MemTr(kvs)
+    setter = lambda k,v : transaction.set(k,v)
+    getter = lambda k :   transaction.get_commited(k)
+    while(1):
+        transaction.begin()
+        try:
+            target_transaction(setter, getter)
+            if transaction.commit() == True:
+                return transaction.cache
+        except AbortException:
+            continue
 
 if __name__ == '__main__':
     mc = memcache.Client(['127.0.0.1:11211'])
-    @transaction(mc)
-    def _(s, g):
+    def init(s, g):
         s('counter',0)
-    for i in range(10000):
-        @transaction(mc)
-        def _(setter, getter):
-            d = getter('counter')
-            setter('counter', d+1)
-    @transaction(mc)
-    def _(setter, getter):
+    def incr(setter, getter):
         d = getter('counter')
-    assert(_['counter'] == 10000)
+        setter('counter', d+1)
+    result = rr_transaction(mc, init)
+    print result['counter']
+    for i in range(10000):
+        result = rr_transaction(mc, incr)
+    print result['counter']
+
+
+
+
